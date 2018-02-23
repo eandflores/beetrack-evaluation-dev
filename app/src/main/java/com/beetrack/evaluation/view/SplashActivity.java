bd178e8d3c54bbba6f7c0578911830208cc87d57
Edgardo Andres <eandres@bitbucket.org>
2018-02-22 23:57:06 -0300

    feature/splash

---
+++ app/src/main/java/com/beetrack/evaluation/view/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.beetrack.evaluation.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        launchArticles();
+    }
+
+    public void launchArticles() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
